48148e6062ffb9a007add2ac095c026fe7bc7d6a
Ahmad Kayyali <kayyali18@gmail.com>
2019-01-18 23:22:38 -0700

    Ad PR template and barebones index file

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const inquirer = require("inquirer");
+
+let questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What's your name?"
+  }
+];
+
+inquirer.prompt(questions).then(answers => {
+  console.log(`Hello ${answers.name}!`);
+});
